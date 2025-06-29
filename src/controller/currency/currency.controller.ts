f0cf7e9fdebad04de651f5dfe02e3d4dc770eb1a
asohyannick <asohyannick3@gmail.com>
2025-06-29 16:34:55 -0700

    User can now fetch and view a single currency created on our expense tracker application successfully because this API endpoint has been created and implemented successfully on the backend!

--- src/controller/currency/currency.controller.ts
+++ src/controller/currency/currency.controller.ts
@@ -4,7 +4,9 @@ import createCurrency from '../../service/impl/currency/createCurrency/createCur
 import globalValidator from '../../middleware/globalValidator/globalValidator';
 import { validateCreatedCurrency } from '../../util/validator';
 import showCurrencies from '../../service/impl/currency/showCurrencies/showCurrencies.impl';
+import showCurrency from '../../service/impl/currency/showCurrency/showCurrency.impl';
 const router = express.Router();
 router.post('/create-currency', authenticationToken, globalValidator(validateCreatedCurrency), createCurrency);
 router.get('/show-currencies', authenticationToken, showCurrencies);
+router.get('/show-currency/:id', authenticationToken, showCurrency);
 export default router;
