bd48417862679ea6a135eac9bacde407ab796540
asohyannick <asohyannick3@gmail.com>
2025-06-30 08:08:33 -0700

    User can now fetch and view a single feedback message he or she has sent to us on our expense tracker application successfully because this API endpoint has been created and implemented successfully on our backend application

---
+++ src/service/impl/feedback/showFeedback/showFeedback.impl.ts
@@ -0,0 +1,25 @@
+import Feedback from "../../../../model/feedback/feedback.model";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+const showFeedback = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const feedback = await Feedback.findById(id);
+        if (!feedback) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "Feedback doesn't exist!" });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Feedback has been fetched successfully!",
+            feedback
+        });
+    } catch (error) {
+        console.error('Error occurred!', error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+
+export default showFeedback;
