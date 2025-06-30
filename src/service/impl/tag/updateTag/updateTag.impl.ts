5534a83acd489c379266e7d7904c90805e774c10
asohyannick <asohyannick3@gmail.com>
2025-06-29 17:17:04 -0700

    User can now edit and update a single tag message created on our expense tracker application successfully because this API endpoint has been created and implemented successfully on the backend!

---
+++ src/service/impl/tag/updateTag/updateTag.impl.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Tag from "../../../../model/tag/tag.model";
+const updateTag = async (req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const { name, description, color } = req.body;
+        const tag = await Tag.findByIdAndUpdate(id, { name, description, color }, { new: true, runValidators: true });
+        if (!tag) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: "Tag doesn't exist!" });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Tag has been updated successfully!",
+            tag
+        });
+    } catch (error) {
+        console.error("Error occurred!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+export default updateTag;
