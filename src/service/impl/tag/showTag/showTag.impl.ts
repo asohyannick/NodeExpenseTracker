53476f63ccc5924d3f8125c872948c7ac134ea8f
asohyannick <asohyannick3@gmail.com>
2025-06-29 17:13:07 -0700

    User can now fetch and view a single tag message created on our expense tracker application successfully because this API endpoint has been created and implemented successfully on the backend!

---
+++ src/service/impl/tag/showTag/showTag.impl.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import Tag from "../../../../model/tag/tag.model";
+const showTag = async(req: Request, res: Response): Promise<Response> => {
+    try {
+        const { id } = req.params;
+        const tag = await Tag.findById(id);
+        if (!tag) {
+            return res.status(StatusCodes.NOT_FOUND).json({message: "Tag doesn't exist!"});
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Tag has been fetched successfully!",
+            tag
+        });
+    } catch (error) {
+        console.error("Error occurred!", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            message: "Something went wrong!",
+            error: error instanceof Error ? error.message : "Unknown Error",
+        });
+    }
+}
+export default showTag;
